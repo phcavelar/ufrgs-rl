c61c4e02536de0843927f045dfed10b30489df59
phcavelar <9154669+phcavelar@users.noreply.github.com>
2019-09-23 21:45:31 -0300

    Analysis exercises done so far (2, 3 and 6)

--- berkeley/analysis.py
+++ berkeley/analysis.py
@@ -19,50 +19,70 @@
 # Set the given parameters to obtain the specified policies through
 # value iteration.
 
+# Aluno: 286120
+
 def question2():
+    """If there is no risk in crossing the bridge and
+    the discount isn't too high, the larger reawrd will be preferred
+    """
     answerDiscount = 0.9
-    answerNoise = 0.2
+    answerNoise = 0.0
     return answerDiscount, answerNoise
 
 def question3a():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    """ Discount is so high that the longer path isn't worth it"""
+    answerDiscount = 0.1
+    answerNoise = 0
+    answerLivingReward = 0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3b():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    """Discount is so high that the longer path isn't worth it, but
+    there is noise during training and a reward for living,
+    such that the bridge is avoided.
+    """
+    answerDiscount = 0.1
+    answerNoise = 0.1
+    answerLivingReward = 0.1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3c():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    """ Discount is low so that the longer path is still worth it
+    """
+    answerDiscount = 0.9
+    answerNoise = 0
+    answerLivingReward = 0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3d():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    """ Discount is low and there is noise, so that the bridge is avoided
+    but the larger reward is preferred.
+    """
+    answerDiscount = 0.9
+    answerNoise = 0.1
+    answerLivingReward = 0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3e():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    """ Reward for living is higher than any of the terminal states"""
+    answerDiscount = 1
+    answerNoise = 0
+    answerLivingReward = 11
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question6():
+    """ The Reward is either need too many episodes being propagated correctly,
+    which is hard for a low epsilon, or a high enough number of episodes that
+    exploit it are too few, which is too hard for a high epsilon 
+    """
     answerEpsilon = None
     answerLearningRate = None
-    return answerEpsilon, answerLearningRate
+    return "NOT POSSIBLE" #answerEpsilon, answerLearningRate
     # If not possible, return 'NOT POSSIBLE'
 
 if __name__ == '__main__':
