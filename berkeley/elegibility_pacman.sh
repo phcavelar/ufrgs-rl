2d63b152c2104e5ed736c04af03851e56589ef4b
phcavelar <9154669+phcavelar@users.noreply.github.com>
2019-10-07 22:02:49 -0300

    Added echo to file

--- berkeley/elegibility_pacman.sh
+++ berkeley/elegibility_pacman.sh
@@ -2,6 +2,7 @@ echo " " > "sarsa_pacman.txt"
 for LAMBDA in 0 0.1 0.3 0.5 0.7 0.9 1.0
 do
   echo "lambda=${LAMBDA}"
+  echo "lambda=${LAMBDA}" >> "sarsa_pacman.txt"
   python2 pacman.py -p PacmanSarsaAgent -x 2000 -n 2010 -l smallGrid -a lamda=${LAMBDA} >> "sarsa_pacman.txt"
   echo " " >> "sarsa_pacman.txt"
   echo " " >> "sarsa_pacman.txt"
