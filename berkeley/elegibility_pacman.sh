8f5384ceabcffe523cad9ae4fce5892c44dc7df9
phcavelar <9154669+phcavelar@users.noreply.github.com>
2019-10-07 22:01:50 -0300

    rename elegibility to elegibility_grid and created a script to generate test results for pacman

---
+++ berkeley/elegibility_pacman.sh
@@ -0,0 +1,13 @@
+echo " " > "sarsa_pacman.txt"
+for LAMBDA in 0 0.1 0.3 0.5 0.7 0.9 1.0
+do
+  echo "lambda=${LAMBDA}"
+  python2 pacman.py -p PacmanSarsaAgent -x 2000 -n 2010 -l smallGrid -a lamda=${LAMBDA} >> "sarsa_pacman.txt"
+  echo " " >> "sarsa_pacman.txt"
+  echo " " >> "sarsa_pacman.txt"
+done
+
+
+
+
+
